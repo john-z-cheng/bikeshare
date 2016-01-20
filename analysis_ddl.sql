60eb6f07b1ba9531a8a107a3bda9c063dacfed4d
John Cheng <john@VirtualBox.com>
2016-01-19 21:01:00 -0700

    calculate distance between trip stations

--- analysis_ddl.sql
+++ analysis_ddl.sql
@@ -11,3 +11,11 @@ CREATE TABLE trip_pairs (
 
 INSERT INTO trip_pairs (start_id, stop_id) 
 SELECT DISTINCT start_id, stop_id FROM trips;
+
+update trip_pairs set distance=sq.distance
+FROM (select tp.id as id,
+ st_distance_sphere(x.geom, y.geom) as distance,
+ tp.start_id, tp.stop_id 
+from trip_pairs tp, stations x, stations y
+where x.id=tp.start_id and y.id=tp.stop_id) as sq
+where trip_pairs.start_id=sq.start_id and trip_pairs.stop_id=sq.stop_id;
