94d4e71af61f9ccb014ac507431b89dfb1b20f41
John Cheng <john@VirtualBox.com>
2016-01-23 14:14:54 -0700

    associate reverse trip with forward trip

--- analysis_ddl.sql
+++ analysis_ddl.sql
@@ -12,6 +12,7 @@ CREATE TABLE trip_pairs (
 INSERT INTO trip_pairs (start_id, stop_id) 
 SELECT DISTINCT start_id, stop_id FROM trips;
 
+-- calculate the distances
 update trip_pairs set distance=sq.distance
 FROM (select tp.id as id,
  st_distance_sphere(x.geom, y.geom) as distance,
@@ -19,3 +20,11 @@ FROM (select tp.id as id,
 from trip_pairs tp, stations x, stations y
 where x.id=tp.start_id and y.id=tp.stop_id) as sq
 where trip_pairs.start_id=sq.start_id and trip_pairs.stop_id=sq.stop_id;
+
+-- associate the reverse trip to the forward trip
+update trip_pairs set reverse_pair_id=x.rp_id
+FROM (select tp.id as id, rp.id as rp_id
+from trip_pairs tp, trip_pairs rp
+where tp.start_id=rp.stop_id and tp.stop_id=rp.start_id) x
+where x.id=trip_pairs.id;
+
